e6c0176a46ee58c75b4baa34e6b02a92c9099ee3
lexcion <31966176+lexcion@users.noreply.github.com>
2022-04-21 19:26:05 -0700

    part 4 and 6

---
+++ MarkdownParseTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void markdownTestFile1() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+    links = parse.getLinks(Files.readString(Path.of("test-file.md")));
+        assertEquals(links.size(), 2);
+        assertEquals(links.get(0), "https://something.com");
+        assertEquals(links.get(1), "some-thing.html");
+    }
+
+    @Test
+    public void markdownTestFile2() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+    links = parse.getLinks(Files.readString(Path.of("test-file-2.md")));
+        assertEquals(links.size(), 2);
+        assertEquals(links.get(0), "https://something.com");
+        assertEquals(links.get(1), "some-thing.html");
+    }
+
+    @Test
+    public void markdownTestFile3() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+    links = parse.getLinks(Files.readString(Path.of("test-file-3.md")));
+        assertEquals(links.size(), 1);
+        assertEquals(links.get(0), "some-thing.html");
+    }
+
+    @Test
+    public void markdownTestFile4() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+    links = parse.getLinks(Files.readString(Path.of("test-file-4.md")));
+        assertEquals(links.size(), 1);
+        assertEquals(links.get(0), "https://something.com");
+    }
+}
